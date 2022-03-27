09a5469116c6df17c8158d760cf0f12b96b11587
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2022-03-28 01:54:24 +0500

    create views

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,8 +2,8 @@ from dataclasses import field
 from rest_framework import serializers
 from .models import Post
 
-class  Postserializer(serializers.ModelSerilaizer):
+class  Postserializer(serializers.ModelSerializer):
     
     class Meta:
         model = Post
-        field=('id','author','title','body','created_date')
+        fields = ('id','author','title','body','create_date',)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+from posts.serializers import Postserializer
+
+from .models import Post
+
+
+class PostList(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = Postserializer
+    
+class PostDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = Postserializer
+    
